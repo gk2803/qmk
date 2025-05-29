919918405e8914d1aadac16e9fa15843f0a45057
gk2803 <georkyri7@gmail.com>
2025-05-29 23:52:24 +0300

    removed the windowmanager specific keybindings

--- keyboards/crkbd/keymaps/gk2803/keymap.c
+++ keyboards/crkbd/keymaps/gk2803/keymap.c
@@ -3,18 +3,28 @@
 #define _BASE 0
 #define _NUM 1
 #define _SYM 2
-#define _WM 3
+#define _FK 3
 
 
 /* the modifiers argument to the One Shot Modifier (OSM) must be prefixed with MOD_ instead of KC_  */
 /* OSM means you can tap the modifier once and it will be held until the next keystroke */
+
+
 #define my_CTRL OSM(MOD_LCTL | MOD_RCTL)
 #define my_SFT OSM(MOD_LSFT | MOD_RSFT)
 #define my_ALT OSM(MOD_LALT | MOD_RALT)
+
+
 /* #define hrm_GUI LGUI_T(KC_QUOT) */
 
-#define SCLN_TL LT(_WM, KC_SCLN)
+/* LT stands for Layer tapping. So LT_QUOTE is used as a layer on hold and as a quote
+ character on tap.
+I get some false when using this key. For example when fast typing the word "don't",
+it will trigger the Layer _WM and make my screen change to another pane.*/
 
+
+#define LT_NUM_ENT LT(_NUM, KC_ENT)
+#define LT_SYM_SP LT(_SYM, KC_SPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
@@ -23,22 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   //,-------------------------------------------------------------.           ,-----------------------------------------------------------.
      KC_ESC,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_PGUP,         KC_CAPS,       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-     KC_TAB ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PGDN,          KC_T,         KC_H,    KC_J,    KC_K,    KC_L, SCLN_TL, KC_QUOT,
+     KC_TAB ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PGDN,          KC_T,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
      my_SFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  my_SFT,
   //|--------+--------+--------+--------+--------+--------+--------|             |--------+--------+--------+--------+--------+--------+-----|
-                                        MO(_SYM), my_CTRL,  KC_SPC,              KC_ENT, KC_LALT, MO(_NUM)
+                                        KC_LGUI, my_CTRL,  LT_SYM_SP,              LT_NUM_ENT, my_ALT, MO(_FK)
                                       //`--------------------------'             `--------------------------'
 
   ), 
 
     [_NUM] = LAYOUT_split_3x6_3_ex2(
   //,-------------------------------------------------------------.            ,-------------------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_T,            KC_T,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+       KC_TAB,XXXXXXX,XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,            KC_T,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-       KC_LCTL, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_PIPE,   KC_T,            KC_T,     XXXXXXX, XXXXXXX, KC_UP,  XXXXXXX, XXXXXXX, XXXXXXX,
+       KC_LCTL, KC_1,   KC_2,   KC_3,    KC_4,      KC_5,   XXXXXXX,            KC_T,    XXXXXXX,  KC_6,    KC_7,    KC_8,   KC_9,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_TILD, XXXXXXX, KC_LCBR, KC_RCBR, KC_BSLS,                              XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              XXXXXXX, KC_0,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,           KC_ENT,   MO(3), KC_RALT
                                       //`--------------------------'          `--------------------------'
@@ -46,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 
     [_SYM] = LAYOUT_split_3x6_3_ex2(
   //,-------------------------------------------------------------.            ,------------------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,   KC_T,            KC_T,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, XXXXXXX, XXXXXXX, KC_PIPE,  KC_BSLS, XXXXXXX,   XXXXXXX,        KC_T,     KC_AMPR, KC_CIRC, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_T,            KC_T,     KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_LCTL, KC_EXLM, KC_AT, KC_HASH, KC_DLR, XXXXXXX,   XXXXXXX,            KC_T,     KC_MINS,  KC_UNDS, KC_LCBR, KC_RCBR, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD, KC_PIPE,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_TILD, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  KC_SPC,           KC_ENT, _______, KC_RALT
                                       //`--------------------------'          `--------------------------'
@@ -58,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     
     /* https://docs.qmk.fm/feature_advanced_keycodes */
 
-    [_WM] = LAYOUT_split_3x6_3_ex2(
-  //,---------------------------------------------------------------------------.      ,----------------------------------------------------------------.
-      KC_TAB , LGUI(KC_1), LGUI(KC_2), LGUI(KC_3),  LGUI(KC_4), LGUI(KC_5),  KC_T,      KC_T,     LGUI(KC_6), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+-----------+-----------+-----------+------------+-----------|                            |--------+--------+--------+--------+--------+--------|
-     LGUI(KC_TAB),XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_T,            KC_T,     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+    [_FK] = LAYOUT_split_3x6_3_ex2(
+  //,-------------------------------------------------------------.            ,------------------------------------------------------------.
+       KC_TAB, KC_F1,  KC_F2,   KC_F3 ,  KC_F4, XXXXXXX,   XXXXXXX,        KC_T,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LCTL, KC_F5,  KC_F6,  KC_F7 ,   KC_F8, XXXXXXX,   XXXXXXX,        KC_T,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                            |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_F9,  KC_F10, KC_F11,   KC_F12, XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX ,XXXXXXX, XXXXXXX,   XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,           KC_ENT, _______, LCTL(KC_BSLS)
+                                          KC_LGUI,   MO(3),  KC_SPC,           KC_ENT, _______, KC_RALT
                                       //`--------------------------'          `--------------------------'
   )
 };
@@ -83,19 +93,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   recommended at the official qmk discord server by user sigprof. 
  */
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case OSL(_WM):  // Override hold behavior for OSL(_NUM)
-            if (!record->tap.count) {  // If it's a hold action
-                if (record->event.pressed) {
-                    register_code(KC_LGUI);  // Hold = GUI
-                } else {
-                    unregister_code(KC_LGUI);
-                }
-                return false;  // Prevent QMK from running its default OSL code
-            }
-            return true;  // Tap = Normal OSL behavior
-    }
-    return true;  // Let other keys behave normally
-}
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*     switch (keycode) { */
+/*         case OSL(_WM):  // Override hold behavior for OSL(_NUM) */
+/*             if (!record->tap.count) {  // If it's a hold action */
+/*                 if (record->event.pressed) { */
+/*                     register_code(KC_LGUI);  // Hold = GUI */
+/*                 } else { */
+/*                     unregister_code(KC_LGUI); */
+/*                 } */
+/*                 return false;  // Prevent QMK from running its default OSL code */
+/*             } */
+/*             return true;  // Tap = Normal OSL behavior */
+/*     } */
+/*     return true;  // Let other keys behave normally */
+/* } */
 
